77238c6014e0585432ed2fbb9b039d0714771967
charlotte-hues <hues.charlotte@gmail.com>
2020-04-28 22:09:12 +0100

    Updates

---
+++ precache-manifest.b441c5495e133df5f7f778b0eb4d78d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c461569d32149c19c8d72397337c11",
+    "url": "/tile-pattern-react/index.html"
+  },
+  {
+    "revision": "bb627c0c05a4cf130fa2",
+    "url": "/tile-pattern-react/static/css/main.5490df0e.chunk.css"
+  },
+  {
+    "revision": "c24b0b6cd9d1f348aa0c",
+    "url": "/tile-pattern-react/static/js/2.45b54284.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tile-pattern-react/static/js/2.45b54284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb627c0c05a4cf130fa2",
+    "url": "/tile-pattern-react/static/js/main.2b8b65c4.chunk.js"
+  },
+  {
+    "revision": "cff6551a42d582b78aec",
+    "url": "/tile-pattern-react/static/js/runtime-main.e2209b31.js"
+  }
+]);
