874690638b9c350c496ba4ddce82c7829806321f
charlotte-hues <hues.charlotte@gmail.com>
2020-05-19 07:21:01 +0100

    trying to fix loose object so i can deploy

---
+++ build/precache-manifest.0a9b15e9b231762ca41bdcec0ddf2a69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc937fde4ceee64addcae8826fedb429",
+    "url": "/the-infinite-coloring-book/index.html"
+  },
+  {
+    "revision": "47855820c10e207cda23",
+    "url": "/the-infinite-coloring-book/static/js/2.2a741e79.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/the-infinite-coloring-book/static/js/2.2a741e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d706e6da85afbd42a9",
+    "url": "/the-infinite-coloring-book/static/js/main.ba1f5992.chunk.js"
+  },
+  {
+    "revision": "871ba9dd332e56b34ef1",
+    "url": "/the-infinite-coloring-book/static/js/runtime-main.ebc1504f.js"
+  }
+]);
