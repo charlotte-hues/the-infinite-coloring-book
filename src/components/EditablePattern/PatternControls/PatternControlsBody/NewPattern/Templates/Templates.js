8aed77a9e64a06cbfc496f5b65cb94ddce902575
charlotte-hues <hues.charlotte@gmail.com>
2020-05-23 17:46:02 +0100

    added the first template - yoda - it's not connected yet so it doesn't do anything

---
+++ src/components/EditablePattern/PatternControls/PatternControlsBody/NewPattern/Templates/Templates.js
@@ -0,0 +1,35 @@
+const patternTemplates = {
+  yoda: {
+    patterns: [
+      { num: 13, locked: false },
+      { num: 13, locked: false },
+      { num: 13, locked: false },
+      { num: 13, locked: false },
+      { num: 13, locked: false },
+      { num: 29, locked: false },
+      { num: 32, locked: false },
+      { num: 18, locked: false },
+      { num: 37, locked: false },
+      { num: 23, locked: false },
+      { num: 35, locked: false },
+      { num: 22, locked: false },
+      { num: 44, locked: false },
+      { num: 22, locked: false },
+      { num: 34, locked: false },
+      { num: 1, locked: false },
+      { num: 35, locked: false },
+      { num: 19, locked: false },
+      { num: 33, locked: false },
+      { num: 0, locked: false },
+      { num: 13, locked: false },
+      { num: 13, locked: false },
+      { num: 6, locked: false },
+      { num: 13, locked: false },
+      { num: 13, locked: false }
+    ],
+    columns: 5,
+    complexity: 1
+  }
+};
+
+export default patternTemplates;
