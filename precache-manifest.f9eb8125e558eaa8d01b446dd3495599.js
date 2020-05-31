260f289f308208e934f1e0532291c927cf7bc929
charlotte-hues <hues.charlotte@gmail.com>
2020-05-31 19:14:57 +0100

    Updates

---
+++ precache-manifest.f9eb8125e558eaa8d01b446dd3495599.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463afe417ab3bfe1a7013eeac1e37569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70beb0f4935ff31b56b7",
+    "url": "/static/js/2.9ebb744c.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.9ebb744c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b15f07a6445a37c81e66",
+    "url": "/static/js/main.66f750f8.chunk.js"
+  },
+  {
+    "revision": "11ec5cd73506e0471dd3",
+    "url": "/static/js/runtime-main.ddf812bd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/the-infinite-coloring-book/precache-manifest.0e060fb11a7beb75da65b228dc2fd112.js"
+  "/precache-manifest.f9eb8125e558eaa8d01b446dd3495599.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/the-infinite-coloring-book/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
